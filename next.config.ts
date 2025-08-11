b34d7fd4cbff9860797ab359d9de27e0f4239917
Abdul-Majeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-08-11 16:30:19 +0000

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@prisma/client", "prisma"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      if (Array.isArray(config.externals)) {
+        config.externals.push("@prisma/client");
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
